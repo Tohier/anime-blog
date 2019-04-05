607743e9f0567ac6fc139c3101e6b3e4da71873f
Tohier <tohierdreyer17@gmail,com>
2019-04-05 16:32:18 +0200

    added the register and sign

--- app.js
+++ app.js
@@ -6,14 +6,8 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-// var routes = [
-//   require('./routes/index'),
-//   require('./routes/users'),
-//   require('./routes/create'),
-//   require('./routes/view'),
-//   require('./routes/update'),
-//   require('./routes/delete'),
-// ];
+var registerRouter = require("./routes/register");
+
 var app = express();
 
 // view engine setup
@@ -28,6 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/register", registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/register.js
+++ routes/register.js
@@ -2,3 +2,61 @@ var express = require("express");
 var router = express.Router();
 var Posts = require("../db.json");
 var request = require("request");
+
+// getting the register page
+router.get('/', function (req, res, next) {
+    res.render('register', {
+        title: "Register",
+        thisError: req.app.locals.regError,
+    });
+});
+
+// create a new user
+router.post('/', function (req, res, next) {
+
+    // used username
+    var usernameUsed;
+
+    // sets ID to last ID in users +1
+    var id = users[users.length - 1].id;
+    id = Number(id) + 1;
+
+    // to check if the username is already used 
+    var logUser = req.body.username;
+
+    // checking through users for used names
+    for (var i = 0; i < users.length; i++) {
+        // check if usename already exists  
+        if (logUser == users[i].username) {
+            usernameUsed = true;
+            console.log(usernameUsed);
+        }
+    }
+    // if username is not available create a new membership account
+    if (usernameUsed != true) {
+        request({
+            url: 'http://localhost:8080/users',
+            method: 'Posts',
+            form: {
+                id: id,
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+            },
+            function(error, response, body) {
+                res.render('register', { message: 'Account Created!' });
+            }
+        });
+        req.app.locals.regError = 'Registration Succesful'
+
+        // goes to sign in page after you register
+        res.redirect('/sign-in');
+    }
+    // if user is already used (user name taken)
+    else if (usernameUsed == true) {
+        req.app.locals.regError = 'Username Taken';
+    }
+    res.redirect('/sign-in');
+})
+
+module.exports = router; 
